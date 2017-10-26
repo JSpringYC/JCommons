5340fc0ff94c8c712a7a38fa6f46e8f2eef109d2
佳木流泉 <JSpringYC@gmail.com>
2017-10-26 15:39:22 +0800

    更新Systems类

---
+++ src/main/java/com/jiangyc/jcommons/util/Systems.java
@@ -0,0 +1,37 @@
+package com.jiangyc.jcommons.util;
+
+public class Systems {
+    // ********************
+    //  Operating System
+    // ********************
+    /**
+     * 系统名称
+     */
+    public static final String OS_NAME = System.getProperty("os.name");
+    /**
+     * 系统版本
+     */
+    public static final String OS_VERSION = System.getProperty("os.version");
+    /**
+     * 系统架构
+     */
+    public static final String OS_ARCH = System.getProperty("os.arch");
+    /**
+     * 是否为Windows系统
+     */
+    public static final boolean IS_WINDOWS = OS_NAME.toLowerCase().contains("windows");
+
+    // ********************
+    //  User
+    // ********************
+    public static final String USER_NAME = System.getProperty("user.name");
+    public static final String USER_HOME = System.getProperty("user.home");
+    public static final String USER_COUNTRY = System.getProperty("user.country");
+    public static final String USER_LANGUAGE = System.getProperty("user.language");
+    public static final String USER_TIMEZONE = System.getProperty("user.timezone");
+    // ********************
+    //  Java
+    // ********************
+    public static final String JAVA_VERSION = System.getProperty("java.version");
+
+}
