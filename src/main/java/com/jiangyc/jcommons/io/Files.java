745fabf922085da91921bcc283c31a08ecbb8ad2
佳木流泉 <JSpringYC@gmail.com>
2017-10-28 13:46:01 +0800

    更新代码注释

--- src/main/java/com/jiangyc/jcommons/io/Files.java
+++ src/main/java/com/jiangyc/jcommons/io/Files.java
@@ -3,11 +3,52 @@ package com.jiangyc.jcommons.io;
 import com.jiangyc.jcommons.util.Asserts;
 
 import java.io.*;
-
 import java.net.URI;
 
+/**
+ * 文件操作工具类，提供了一系列文件操作的静态方法。
+ */
 public class Files {
 
+    // ********************
+    //  文件操作
+    // ********************
+
+    /**
+     * 删除文件或文件夹
+     * @param file 要删除的文件
+     * @param r 是否对文件夹进行递归操作
+     * @return 是否成功删除该文件
+     */
+    public static boolean delete(File file, boolean r) {
+        Asserts.fileExist(file, null);
+
+        // 删除文件
+        if (file.isFile()) {
+            return file.delete();
+        }
+
+        File[] subFiles = file.listFiles();
+
+        // 递归删除
+        if (subFiles != null && subFiles.length > 0) {
+            if (!r) {
+                return false;
+            }
+            // 递归删除子文件
+            for (File subFile : file.listFiles()) {
+                if (!delete(file, true)) {
+                    return false;
+                }
+            }
+        }
+        // 最后删除文件
+        return file.delete();
+    }
+
+    // ********************
+    //  流操作
+    // ********************
     public static InputStream asInputStream(Object input) throws IOException {
         Asserts.notNull(input);
 
--- src/main/java/com/jiangyc/jcommons/util/Asserts.java
+++ src/main/java/com/jiangyc/jcommons/util/Asserts.java
@@ -1,13 +1,46 @@
 package com.jiangyc.jcommons.util;
 
+import java.io.File;
+
+/**
+ * 一系列断言的静态方法
+ */
 public class Asserts {
 
+    /**
+     * 参数不能为空
+     * @param o 要断言的对象
+     */
     public static void notNull(Object o) {
         notNull(o, "参数不能为空!");
     }
 
+    /**
+     * 参数不能为空
+     * @param o 要断言的对象
+     * @param message 提示信息
+     */
     public static void notNull(Object o, String message) {
         if (o == null) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    /**
+     * 文件存在
+     * @param file 要断言的文件
+     */
+    public static void fileExist(File file) {
+        fileExist(file, "文件不存在!");
+    }
+
+    /**
+     * 文件存在
+     * @param file 要断言的文件
+     * @param message 提示信息
+     */
+    public static void fileExist(File file, String message) {
+        if (file == null || !file.exists()) {
             throw new IllegalArgumentException(message);
         }
     }
